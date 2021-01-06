9050061c036c5d4dca642dc9c95e82faed63ec58
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-01-06 17:07:36 -0300

    modificamos hook

--- src/hooks/useAtributos.js
+++ src/hooks/useAtributos.js
@@ -1,16 +1,16 @@
-// asigna atributos aleatorios para luego inyectar en el object de los heroes seleccionados para luchar
+// genera atributos aleatorios para los luchadores de la velada, los cuales son 2
 export default function useAtributos() {
   const atributos1 = {
-    damage: Math.floor(Math.random() * 100),
+    damage: Math.floor(Math.random() * 120),
     agility: Math.floor(Math.random() * 70),
-    health: Math.floor(Math.random() * 500),
-    stamina: Math.floor(Math.random() * 140),
+    health: Math.floor(Math.random() * 800),
+    stamina: Math.floor(Math.random() * 200),
   };
   const atributos2 = {
-    damage: Math.floor(Math.random() * 100),
+    damage: Math.floor(Math.random() * 120),
     agility: Math.floor(Math.random() * 70),
-    health: Math.floor(Math.random() * 300),
-    stamina: Math.floor(Math.random() * 140),
+    health: Math.floor(Math.random() * 800),
+    stamina: Math.floor(Math.random() * 200),
   };
 
   return {
