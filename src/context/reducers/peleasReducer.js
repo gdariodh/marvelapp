f962ddc2fa2b99820e8aee6eefffc7f41b635a33
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-01-06 17:06:05 -0300

    modificamos reducer

--- src/context/reducers/peleasReducer.js
+++ src/context/reducers/peleasReducer.js
@@ -1,31 +1,36 @@
 import { LUCHADORES, LUCHADOR_RANKING, RESET_LUCHADORES } from "../../types";
 
 export default function (state, action) {
-
-
   switch (action.type) {
     case LUCHADORES:
       // JSON.stringify para guardar un array con por lo menos 2 objetos
-      localStorage.setItem("luchadores",JSON.stringify(action.payload))
+      localStorage.setItem(
+        "luchadores",
+        // spread operator en JSON.stringify para acumular los luchadores en localStorage
+        JSON.stringify([action.payload, ...state.luchadoresranking])
+      );
       return {
         ...state,
-        luchadores: [...state.luchadores,action.payload],
+        luchadores: [...state.luchadores, action.payload],
       };
 
     case LUCHADOR_RANKING:
-      // JSON.stringify para guardar un array con uno o mas objetos
-      localStorage.setItem("ranking",JSON.stringify([action.payload,...state.luchadoresranking]))
+      localStorage.setItem(
+        "ranking",
+        // spread operator en JSON.stringify para acumular los luchadores en localStorage
+        JSON.stringify([action.payload, ...state.luchadoresranking])
+      );
       return {
         ...state,
         luchadoresranking: [action.payload, ...state.luchadoresranking],
       };
 
     case RESET_LUCHADORES:
-      return{
+      localStorage.removeItem("luchadores");
+      return {
         ...state,
-        luchadores: []
-      }   
-
+        luchadores: [],
+      };
 
     default:
       return state;
