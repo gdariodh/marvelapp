754e0f4abdbdd8c6018a3707d6a25fada6d31280
gdariodh <diazhernandezgabriel2001@gmail.com>
2021-01-06 05:35:07 -0300

    modificamos css

--- src/hooks/useCantidadPeleas.js
+++ src/hooks/useCantidadPeleas.js
@@ -5,13 +5,13 @@ const useCantidadPeleas = () => {
   const [numero_peleas, setNumeroPeleas] = useState(1);
 
   const FiltroUI = () => (
-    <form className='bg-white  rounded flex justify-center '>
+    <form className='rounded flex justify-center w-full '>
       <select
         onChange={(e) => setNumeroPeleas(e.target.value)}
         value={numero_peleas}
-        className='text-md font-semibold mb-2 text-lg'
+        className='text-md py-3 px-8 md:px-12 shadow md:shadow-md font-semibold mb-2 text-lg focus:outline-none'
         >
-        <option className='text-md font-semibold mb-2' value={1}>--Quieres mas de una pelea?--</option>
+        <option className='text-md font-semibold mb-2' value={1}>¿Quieres mas de una pelea?</option>
         <option className='text-md font-semibold mb-2' value={2}>2 Peleas</option>
         <option className='text-md font-semibold mb-2' value={3}>3 Peleas</option>
       </select>
